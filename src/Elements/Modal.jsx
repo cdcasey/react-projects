3b92ec2d99294c046b97e5152ab49050651a06a9
Chris Casey <cdcasey@gmail.com>
2019-05-05 23:20:17 -0500

    CSS utils

--- src/Elements/Modal.jsx
+++ src/Elements/Modal.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-import { Portal } from 'Utilities';
+import { Portal, absolute } from 'Utilities';
 import { Card } from './Cards';
 import Icon from './Icon';
 
@@ -27,9 +27,7 @@ export default class Modal extends Component {
 }
 
 const ModalWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
+  ${absolute({})}
   width: 100%;
   height: 100%;
   display: flex;
@@ -45,20 +43,16 @@ const ModalCard = styled(Card)`
 `;
 
 const CloseButton = styled.button`
-  position: absolute;
-  top: 0;
-  right: 0;
+  ${absolute({ x: 'right' })}
   border: none;
   background: transparent;
   padding: 10px;
 `;
 
 const Background = styled.div`
-  position: absolute;
+  ${absolute({})}
   height: 100%;
   width: 100%;
-  top: 0;
-  left: 0;
   background: black;
   opacity: 0.5;
 `;
